598b4e16297ed5c8bfa91fbb6aed92ed29a919b7
biljanabm <bmgrafika@yahoo.com>
2023-05-08 13:29:45 +0200

    Biljana registracija html padajuci meni proba gita

--- backend/src/mysql/initMysql.ts
+++ backend/src/mysql/initMysql.ts
@@ -3,7 +3,7 @@ import mysql from "mysql2";  // u backend smo instalirali npm install mysql2
 export const dbConnection = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
-    password: "123456",  // sifra u workbencu
+    password: "ZlatnoIPlavo.",  // sifra u workbencu
     database: "projekat3"     // ime baze u mySQL
 })
 
