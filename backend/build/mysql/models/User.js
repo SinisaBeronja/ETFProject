3d825f46a9e4cc105d79422715f70b021c4780f4
SinisaBeronja <sinisa.beronja@mts.rs>
2023-05-22 12:34:32 +0200

    edituser ok

--- backend/build/mysql/models/User.js
+++ backend/build/mysql/models/User.js
@@ -3,6 +3,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
 class User {
+    constructor() {
+        this.idRukovodioca = 0;
+        this.username = "";
+        this.lozinka = "";
+        this.ime = "";
+        this.prezime = "";
+        this.datumRodjenja = "";
+        this.mailRukovodioca = "";
+        this.telefon = "";
+        this.pregledPodnetih = "";
+        this.idInstitucije = 0;
+        this.nazivInstSrp = "";
+    }
 }
 exports.User = User;
 //# sourceMappingURL=User.js.map
