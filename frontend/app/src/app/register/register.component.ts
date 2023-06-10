a79eeb8dd607e34fedd398470973f879bb59cba4
biljanabm <bmgrafika@yahoo.com>
2023-06-10 13:47:44 +0200

    Biljana css za novu notrifikaciju i mala izmena u register

--- frontend/app/src/app/register/register.component.ts
+++ frontend/app/src/app/register/register.component.ts
@@ -41,7 +41,7 @@ export class RegisterComponent implements OnInit {
     idInstitucije: number = 1
 
     regexIme = new RegExp("^[A-Z][a-z]+$");
-    regexPrezime = new RegExp("^[A-Z][a-z]+$");
+    regexPrezime = new RegExp("/^[A-Z]/")
     regexUsername = new RegExp("^[a-z]+\d*\w*$");
     regexlozinka = new RegExp(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/);
     regexDatumRodjenja = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2}\.)$/gm;
